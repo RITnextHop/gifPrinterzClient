65c37a043d08014f377a70bbf2e26e8935f97a97
bwoolf <bwoolf@cisco.com>
2018-03-04 13:23:41 -0500

    no ipython

--- gifPrinterClient/signals.py
+++ gifPrinterClient/signals.py
@@ -2,7 +2,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Upload
 import ftplib
-from IPython import embed
 
 @receiver(post_save, sender=Upload)
 def print_gif(sender, instance, **kwargs):
@@ -11,7 +10,6 @@ def print_gif(sender, instance, **kwargs):
     gif_path = 'media/'+str(instance.gif)
     gif_file = open(gif_path,'rb')
     cmd = 'STOR pub/'+str(instance.gif).split('/')[-1]
-    #embed()
     session.storbinary(cmd, gif_file)
     gif_file.close()
     session.quit()
